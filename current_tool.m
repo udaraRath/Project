1ec85f34522cb511ee5b6adfc587309bfa79d82e
udaraRath <udararr@gmail.com>
2017-01-26 09:34:22 +0530

    Add files via upload

--- current_tool.m
+++ current_tool.m
@@ -44,72 +44,13 @@ RAW_QC = feval(['curr_QC_T3_' CONFIG_CURR.curr_qc_t3_ver],RAW_QC, CONFIG_CURR, C
 RAW_QC.beam_vel_map = feval(['curr_bin_mapping_' CONFIG_CURR.curr_bin_map_ver],RAW_QC, CONFIG_CURR, CONFIG_ADCP);
 
 %% convert beam to polar coordinates 
-[RAW_QC] = feval(['curr_beam2polar_' CONFIG_CURR.curr_beam2polar_ver],RAW_QC, CONFIG_ADCP, CONFIG_CURR);
+[PACKET_QC] = feval(['curr_beam2polar_' CONFIG_CURR.curr_beam2polar_ver],RAW_QC, CONFIG_ADCP, CONFIG_CURR);
 
 %% packet data
 % this has been put in the main tool to limit memory requirements
-
 % RAW = feval(['current_packet_data_' CONFIG_CURR.current_packet_data_version], ADCP_RAW, CONFIG_CURR, CONFIG_ADCP);
-[nping, nbin, ~] = size(RAW_QC.beam_vel);
-npoint = CONFIG_CURR.lpacket * CONFIG_ADCP.fs;
-npacket = floor(nping / npoint);
-nbeam  = CONFIG_ADCP.nbeam;
-
-% original order is [nens, nbin, nbeam]
-PACKET_QC.beam_vel = feval(['curr_packet_var_' CONFIG_CURR.curr_packet_var_ver],RAW_QC.beam_vel, npacket, npoint, nbin, nbeam);
-RAW_QC = rmfield(RAW_QC, 'beam_vel');
-PACKET_QC.beam_vel_map = feval(['curr_packet_var_' CONFIG_CURR.curr_packet_var_ver], RAW_QC.beam_vel_map, npacket, npoint, nbin, nbeam);
-RAW_QC = rmfield(RAW_QC, 'beam_vel_map');
-PACKET_QC.QC_T2 = feval(['curr_packet_var_' CONFIG_CURR.curr_packet_var_ver], RAW_QC.QC_T2, npacket, npoint, nbin, nbeam);
-RAW_QC = rmfield(RAW_QC, 'QC_T2');
-% final order is [npacket, npoint, nbin, nbeam]
-
-% original order is [nens, nbin]
-PACKET_QC.Vx = feval(['curr_packet_var_' CONFIG_CURR.curr_packet_var_ver],RAW_QC.Vx, npacket, npoint, nbin);
-RAW_QC = rmfield(RAW_QC, 'Vx');
-PACKET_QC.Vy = feval(['curr_packet_var_' CONFIG_CURR.curr_packet_var_ver], RAW_QC.Vy, npacket, npoint, nbin);
-RAW_QC = rmfield(RAW_QC, 'Vy');
-PACKET_QC.Vz = feval(['curr_packet_var_' CONFIG_CURR.curr_packet_var_ver], RAW_QC.Vz, npacket, npoint, nbin);
-RAW_QC = rmfield(RAW_QC, 'Vz');
-PACKET_QC.error_vel = feval(['curr_packet_var_' CONFIG_CURR.curr_packet_var_ver], RAW_QC.error_vel, npacket, npoint, nbin);
-RAW_QC = rmfield(RAW_QC, 'error_vel');
-PACKET_QC.Ve = feval(['curr_packet_var_' CONFIG_CURR.curr_packet_var_ver], RAW_QC.Ve, npacket, npoint, nbin);
-RAW_QC = rmfield(RAW_QC, 'Ve');
-PACKET_QC.Vn = feval(['curr_packet_var_' CONFIG_CURR.curr_packet_var_ver], RAW_QC.Vn, npacket, npoint, nbin);
-RAW_QC = rmfield(RAW_QC, 'Vn');
-PACKET_QC.Vv = feval(['curr_packet_var_' CONFIG_CURR.curr_packet_var_ver], RAW_QC.Vv, npacket, npoint, nbin);
-RAW_QC = rmfield(RAW_QC, 'Vv');
-PACKET_QC.mag = feval(['curr_packet_var_' CONFIG_CURR.curr_packet_var_ver], RAW_QC.mag, npacket, npoint, nbin);
-RAW_QC = rmfield(RAW_QC, 'mag');
-PACKET_QC.dir = feval(['curr_packet_var_' CONFIG_CURR.curr_packet_var_ver], RAW_QC.dir, npacket, npoint, nbin);
-RAW_QC = rmfield(RAW_QC, 'dir');
-PACKET_QC.QC_T2_4BEAM = feval(['curr_packet_var_' CONFIG_CURR.curr_packet_var_ver], RAW_QC.QC_T2_4BEAM, npacket, npoint, nbin);
-RAW_QC = rmfield(RAW_QC, 'QC_T2_4BEAM');
-PACKET_QC.QC_T2_3BEAM = feval(['curr_packet_var_' CONFIG_CURR.curr_packet_var_ver], RAW_QC.QC_T2_3BEAM, npacket, npoint, nbin);
-RAW_QC = rmfield(RAW_QC, 'QC_T2_3BEAM');
-% final order is [npacket, npoint, nbin]
-
-% original order is [nens, 1]
-PACKET_QC.mtime = feval(['curr_packet_var_' CONFIG_CURR.curr_packet_var_ver], RAW_QC.mtime, npacket, npoint);
-RAW_QC = rmfield(RAW_QC, 'mtime');
-PACKET_QC.roll = feval(['curr_packet_var_' CONFIG_CURR.curr_packet_var_ver], RAW_QC.roll, npacket, npoint);
-RAW_QC = rmfield(RAW_QC, 'roll');
-PACKET_QC.pitch = feval(['curr_packet_var_' CONFIG_CURR.curr_packet_var_ver], RAW_QC.pitch, npacket, npoint);
-RAW_QC = rmfield(RAW_QC, 'pitch');
-PACKET_QC.heading = feval(['curr_packet_var_' CONFIG_CURR.curr_packet_var_ver], RAW_QC.heading, npacket, npoint);
-RAW_QC = rmfield(RAW_QC, 'heading');
-PACKET_QC.depth = feval(['curr_packet_var_' CONFIG_CURR.curr_packet_var_ver], RAW_QC.depth, npacket, npoint);
-RAW_QC = rmfield(RAW_QC, 'depth');
-PACKET_QC.temperature = feval(['curr_packet_var_' CONFIG_CURR.curr_packet_var_ver], RAW_QC.temperature, npacket, npoint);
-RAW_QC = rmfield(RAW_QC, 'temperature');
-PACKET_QC.pressure = feval(['curr_packet_var_' CONFIG_CURR.curr_packet_var_ver], RAW_QC.pressure, npacket, npoint);
-RAW_QC = rmfield(RAW_QC, 'pressure');
-PACKET_QC.AnBit = feval(['curr_packet_var_' CONFIG_CURR.curr_packet_var_ver], RAW_QC.AnBit, npacket, npoint);
-RAW_QC = rmfield(RAW_QC, 'AnBit');
-% final order is [npacket, npoint]
-
 %% define mean velocities, ancillary data and depth averaging 
-CURRENT = feval(['curr_define_' CONFIG_CURR.curr_define_ver], PACKET_QC);
+CURRENT = feval(['curr_define_statistics_' CONFIG_CURR.curr_stat_ver], PACKET_QC);
 
 %% define reference bins
 CURRENT = feval(['curr_define_ref_bin_' CONFIG_CURR.curr_reference_bin_ver], CURRENT,CONFIG_ADCP, CONFIG_CURR);
@@ -121,24 +62,26 @@ CURRENT = feval(['curr_best_heading_' CONFIG_CURR.curr_best_heading_ver], PACKET
 CURRENT = feval(['curr_vel_shear_' CONFIG_CURR.curr_vel_shear_ver], PACKET_QC, CURRENT,CONFIG_ADCP);
 
 %% Define statistics of flood/ebb velocities in synodic months
-CURRENT = feval(['curr_define_statistics_' CONFIG_CURR.curr_define_stat_ver], CURRENT,CONFIG_CURR);
+CURRENT = feval(['curr_synodic_statistics_' CONFIG_CURR.curr_synodic_ver], CURRENT,CONFIG_CURR);
 
 %% calculate joint probability of Magnitude and Direction
 CURRENT = feval(['curr_joint_prob_' CONFIG_CURR.curr_joint_prob_ver], CURRENT,CONFIG_CURR);
 
 %% Calculate power law for flood/ ebb
-CURRENT = feval(['curr_power_law_' CONFIG_CURR.curr_power_law_ver], PACKET_QC,CURRENT);
+CURRENT = feval(['curr_power_law_' CONFIG_CURR.curr_power_law_ver], PACKET_QC,CURRENT, CONFIG_CURR);
 
-%% turbulence analysis
-CONFIG_TURB.vel_bin_width    = CONFIG_CURR.vel_bin_width;
-CONFIG_TURB.lpacket          = CONFIG_CURR.lpacket;
-CONFIG_TURB.start_date       = CONFIG_CURR.start_date;
-TURBULENCE = feval(['turb_analysis_' CONFIG_TURBULENCE.turb_analysis_ver], CURRENT, PACKET_QC, CONFIG_TURB, CONFIG_ADCP);
-TURBULENCE.ref_bin           = CURRENT.ref_bin;
+%% Calculate std of Doppler noise
+CURRENT =feval(['curr_variance_calc_' CONFIG_CURR.curr_var_calc_ver],CURRENT,PACKET_QC, CONFIG_CURR, CONFIG_ADCP);
+
+%% Reynold/ Kinetic energy ane calculate turbulance intensity
+CURRENT =feval(['curr_turb_intensity_' CONFIG_CURR.curr_turb_intensity_ver],CURRENT, PACKET_QC, CONFIG_CURR, CONFIG_ADCP);
+
+%% calculate mean and std of turbulance itensity
+CURRENT =feval(['curr_TI_statistics_' CONFIG_CURR.curr_TI_stat_ver],PACKET_QC, CURRENT, CONFIG_CURR);
 
 %% save all data
 save([CONFIG_ADCP.ADCP_ref '_processed_current_' datestr(now,'yymmddHHMM')], ...
-     'CURRENT', 'TURBULENCE', 'PACKET_QC', 'RAW_QC', 'CONFIG_CURR', 'CONFIG_TURB', 'CONFIG_ADCP', 'STEERING', '-v7.3');
+     'CURRENT', 'PACKET_QC', 'CONFIG_CURR', 'CONFIG_ADCP', 'STEERING', '-v7.3');
 
 %% return to original directory
 cd(present_dir)
